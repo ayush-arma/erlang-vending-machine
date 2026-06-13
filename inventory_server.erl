cf64ffe2e6d7619fca00efd4dd99b441b41edffb
Ayush Sharma <58801471+ayush-arma@users.noreply.github.com>
2026-06-13 23:13:13 +0530

    Furhter fixes

--- inventory_server.erl
+++ inventory_server.erl
@@ -1,7 +1,7 @@
 -module(inventory_server).
--behaviour(gen_server). 
+-behaviour(gen_server).
 
--export([start_link/0,get_price/1,check_stock/1,decrease_stock/1,get_items_below/1]).
+-export([start_link/0, get_price/1, check_stock/1, decrease_stock/1, get_items_below/1]).
 
 -export([
     init/1,
@@ -11,65 +11,57 @@
     code_change/3
 ]).
 
--define(INITIAL_VENDING_STATE,#{
+-define(INITIAL_VENDING_STATE, #{
     coke => {25, 5},
     pepsi => {30, 3},
     water => {15, 10}
 }).
 
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, ?INITIAL_VENDING_STATE, []).
 
-start_link()->
-    gen_server:start_link({local,?MODULE},?MODULE,?INITIAL_VENDING_STATE,[]).
+init(State) ->
+    {ok, State}.
 
-init(State)->
-    {ok,State}.
+get_price(Item) ->
+    gen_server:call(?MODULE, {get_price, Item}).
 
-get_price(Item)->
-    gen_server:call(?MODULE,{get_price,Item}).
+check_stock(Item) ->
+    gen_server:call(?MODULE, {check_stock, Item}).
 
-check_stock(Item)->
-    gen_server:call(?MODULE,{check_stock,Item}).
+get_items_below(PriceLim) ->
+    gen_server:call(?MODULE, {get_items_below, PriceLim}).
 
-get_items_below(PriceLim)->
-    gen_server:call(?MODULE,{get_items_below,PriceLim}).
+decrease_stock(Item) ->
+    gen_server:cast(?MODULE, {decrease_stock, Item}).
 
-decrease_stock(Item)->
-    gen_server:cast(?MODULE,{decrease_stock,Item}).
-
-handle_call({get_price,Item},_From,Map)->
-    {Price,_Quantity} = maps:get(Item, Map, {0,0}),
-    {reply,Price,Map};
-handle_call({check_stock,Item},_From,Map)->
-    {_Price,Quantity} = maps:get(Item, Map, {0,0}),
-    {reply,Quantity,Map};
+handle_call({get_price, Item}, _From, Map) ->
+    {Price, _Quantity} = maps:get(Item, Map, {0, 0}),
+    {reply, Price, Map};
+handle_call({check_stock, Item}, _From, Map) ->
+    {_Price, Quantity} = maps:get(Item, Map, {0, 0}),
+    {reply, Quantity, Map};
 handle_call({get_items_below, PriceLim}, _From, Map) ->
     FilteredMap = maps:filter(
-        fun(_Key, {Price, _Quantity}) -> Price =< PriceLim end, 
+        fun(_Key, {Price, _Quantity}) -> Price =< PriceLim end,
         Map
     ),
     {reply, FilteredMap, Map}.
 
-
-handle_cast({decrease_stock,Item},Map)->
-    {Price,Quantity}=maps:get(Item,Map,{undefined,0}),
-    case Quantity>0 of 
+handle_cast({decrease_stock, Item}, Map) ->
+    {Price, Quantity} = maps:get(Item, Map, {undefined, 0}),
+    case Quantity > 0 of
         false ->
-            {noreply,Map};
-        _Anthing->
-            NewMap=Map#{Item=>{Price,Quantity-1}},
-            {noreply,NewMap}
-        end;
-handle_cast(_,Map)->
-    {noreply,Map}.
+            {noreply, Map};
+        _Anthing ->
+            NewMap = Map#{Item => {Price, Quantity - 1}},
+            {noreply, NewMap}
+    end;
+handle_cast(_, Map) ->
+    {noreply, Map}.
 
 terminate(_Reason, _State) ->
     ok.
-    
+
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
-
-
-
-
-
-
--- vending_machine.erl
+++ vending_machine.erl
@@ -9,29 +9,35 @@
 -export([
     init/1,
     callback_mode/0,
-    idle/3
+    idle/3,
+    coin_inserted/3
+
 ]).
 
-
-callback_mode()->
+callback_mode() ->
     state_functions.
 
-start_link()->
+start_link() ->
     gen_statem:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-select(Item)->
-    gen_statem:call(?MODULE,{select,Item}).
-
+select(Item) ->
+    gen_statem:call(?MODULE, {select, Item}).
 
 init([]) ->
     {ok, idle, #{}}.
 
-insert_coin(Coin)->
-    gen_statem:call(?MODULE, {show_items,Coin}).
+insert_coin(Coin) ->
+    gen_statem:call(?MODULE, {show_items, Coin}).
 
-idle({call, From}, {show_items,CoinValue}, Data) ->
+idle({call, From}, {show_items, CoinValue}, Data) ->
+    io:format("Idle Called ~p ~p ~p",[From,CoinValue,Data]),
     Items = inventory_server:get_items_below(CoinValue),
-    {keep_state, Data,[{reply,From, Items}]}.
+    NewState = #{
+        inserted => CoinValue,
+        available => Items
+    },
+    {next_state, coin_inserted, NewState, [{reply, From, Items}]}.
 
-
-
+coin_inserted({call,From},{Action,ItemName},State)->
+    io:format("User wants to ~p the item ~p ~p ~n", [Action, ItemName, State]),
+    {keep_state, State, [{reply, From, ok}]}.
