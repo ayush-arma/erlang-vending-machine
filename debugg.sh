aaeeb4ecac07e5f66d79bc423b4d7a7f4b8e9aef
Ayush Sharma <58801471+ayush-arma@users.noreply.github.com>
2026-06-14 21:02:08 +0530

    Added timeout

--- debugg.sh
+++ debugg.sh
@@ -5,6 +5,9 @@ rm -f *.dump
 
 erlc *.erl
 
-erl -eval "inventory_server:start_link(), vending_machine:start_link(),vending_machine:insert_coin(40),vending_machine:buy_item(coke). "
+erl -eval "inventory_server:start_link(), 
+vending_machine:start_link(),
+vending_machine:insert_coin(40),
+vending_machine:buy_item(coke). "
 
 
--- vending_machine.erl
+++ vending_machine.erl
@@ -20,7 +20,6 @@ callback_mode() ->
 start_link() ->
     gen_statem:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-
 insert_coin(CoinValue) ->
     gen_statem:call(?MODULE, {insert_coin, CoinValue}).
 
@@ -35,14 +34,22 @@ idle({call, From}, {insert_coin, CoinValue}, 0) ->
 
     AvailableItems = inventory_server:get_items_below(CoinValue),
 
-    {next_state, coin_inserted, CoinValue, [{reply, From, {ok, AvailableItems}}]}.
+    {next_state, coin_inserted, CoinValue, [
+        {reply, From, {ok, AvailableItems}},
+        {timeout, 10000, inactive}
+    ]}.
 
 coin_inserted({call, From}, {insert_coin, CoinValue}, CurrentBalance) ->
     NewBalance = CurrentBalance + CoinValue,
     io:format("Added ~p. Total Balance: ~p~n", [CoinValue, NewBalance]),
-
     AvailableItems = inventory_server:get_items_below(NewBalance),
-    {keep_state, NewBalance, [{reply, From, {ok, AvailableItems}}]};
+    {keep_state, NewBalance, [
+        {reply, From, {ok, AvailableItems}},
+        {timeout, 10000, inactive}
+    ]};
+coin_inserted(timeout, inactive, CurrentBalance) ->
+    io:format("~n⏰ 10 seconds of inactivity! Returning ~p coins to the user.~n", [CurrentBalance]),
+    {next_state, idle, 0};
 coin_inserted({call, From}, {buy, ItemName}, CurrentBalance) ->
     case inventory_server:purchase(ItemName, CurrentBalance) of
         {ok, Change} ->
@@ -54,7 +61,7 @@ coin_inserted({call, From}, {buy, ItemName}, CurrentBalance) ->
         {error, out_of_stock} ->
             io:format("~p is out of stock!~n", [ItemName]),
             {keep_state, CurrentBalance, [{reply, From, {error, out_of_stock}}]};
-        Anything->
-            io:format("~p Something else!~n", [Anything]),
+        Anything ->
+            io:format("~p Received, Unknown behaviour~n", [Anything]),
             {keep_state, CurrentBalance, [{reply, From, {exception}}]}
-        end.
+    end.
