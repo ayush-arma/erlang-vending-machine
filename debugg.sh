9d706cce5cf1a63dc76b852f42eedf99b4c169ec
Ayush Sharma <58801471+ayush-arma@users.noreply.github.com>
2026-06-14 15:39:26 +0530

    Fixed .sh files

--- debugg.sh
+++ debugg.sh
@@ -1,9 +1,11 @@
 
 rm -f *.beam
 
+rm -f *.dump
+
+
 erlc *.erl
 
-
-erl -eval "inventory_server:start_link(), vending_machine:start_link().vending_machine:insert_coin(40),vending_machine:insert_coin(40). "
+erl -eval "inventory_server:start_link(), vending_machine:start_link(),vending_machine:insert_coin(40),vending_machine:buy_item(coke). "
 
 
--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 rm -f *.beam
 
+rm -f *.dump
+
 erlc *.erl
 
 erl -eval "inventory_server:start_link(), vending_machine:start_link()."
