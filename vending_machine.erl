4198295bde40ceea510bbe4a40d20283245b4e96
Ayush Sharma <58801471+ayush-arma@users.noreply.github.com>
2026-06-14 21:33:16 +0530

    Cancel Added

--- vending_machine.erl
+++ vending_machine.erl
@@ -11,8 +11,11 @@
     init/1,
     callback_mode/0,
     idle/3,
+    cancel/0,
     coin_inserted/3
 ]).
+
+-define(TIMEOUT, 100000).
 
 callback_mode() ->
     state_functions.
@@ -26,6 +29,9 @@ insert_coin(CoinValue) ->
 buy_item(ItemName) ->
     gen_statem:call(?MODULE, {buy, ItemName}).
 
+cancel() ->
+    gen_statem:call(?MODULE, cancel).
+
 init([]) ->
     {ok, idle, 0}.
 
@@ -36,19 +42,24 @@ idle({call, From}, {insert_coin, CoinValue}, 0) ->
 
     {next_state, coin_inserted, CoinValue, [
         {reply, From, {ok, AvailableItems}},
-        {timeout, 10000, inactive}
-    ]}.
-
+        {timeout, ?TIMEOUT, inactive}
+    ]};
+idle(_, cancel, _) ->
+    io:format("No coin found, can't cancel.~n"),
+    {next_state, idle, 0}.
 coin_inserted({call, From}, {insert_coin, CoinValue}, CurrentBalance) ->
     NewBalance = CurrentBalance + CoinValue,
     io:format("Added ~p. Total Balance: ~p~n", [CoinValue, NewBalance]),
     AvailableItems = inventory_server:get_items_below(NewBalance),
     {keep_state, NewBalance, [
         {reply, From, {ok, AvailableItems}},
-        {timeout, 10000, inactive}
+        {timeout, ?TIMEOUT, inactive}
     ]};
 coin_inserted(timeout, inactive, CurrentBalance) ->
     io:format("~n⏰ 10 seconds of inactivity! Returning ~p coins to the user.~n", [CurrentBalance]),
+    {next_state, idle, 0};
+coin_inserted({call, _From}, cancel, CurrentBalance) ->
+    io:format("~n⏰ Returning your money:~p:~n", [CurrentBalance]),
     {next_state, idle, 0};
 coin_inserted({call, From}, {buy, ItemName}, CurrentBalance) ->
     case inventory_server:purchase(ItemName, CurrentBalance) of
